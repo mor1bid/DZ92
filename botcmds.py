bb35d221126b7bdf7f53ae59a43d56cccc89e1ca
De <spark49942@gmail.com>
2022-10-26 02:04:03 +0300

    L5-3

--- botcmds.py
+++ botcmds.py
@@ -1,9 +1,10 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import datetime
-from spiek.py import *
+from spiek import *
 
 def howareu(update: Update, context: CallbackContext):
+    log(update, context)
     update.message.reply_text(f'How bout conquering the world, {update.effective_user.first_name}?')
 
 def hello(update: Update, context: CallbackContext):
@@ -11,15 +12,18 @@ def hello(update: Update, context: CallbackContext):
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 def summe(update: Update, context: CallbackContext):
+    log(update, context)
     # update.message.reply_text(f'Enter 2 digits please:')
     msg = update.message.text
     digs = msg.split()
     numa = int(digs[1])
     numb = int(digs[2])
-    update.message.reply_text(f'{numb} + {numa} = {numb+numa}')
+    update.message.reply_text(f'Here ye go: {numb} + {numa} = {numb+numa}')
 
 def time(update: Update, context: CallbackContext):
+    log(update, context)
     update.message.reply_text(f'Its {datetime.datetime.now().time()}')
 
 def helpme(update: Update, context: CallbackContext):
+    log(update, context)
     update.message.reply_text(f'Here ye go:\n/hi\n/howareu\n/time\n/summe\n')
--- spiek.py
+++ spiek.py
@@ -3,4 +3,4 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 
 def log(update: Update, context: CallbackContext):
     with open('db.csv', 'a') as file:
-        file.write(f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}')
+        file.write(f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}\n')
