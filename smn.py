dd9917e273dc87b725759f10e46383791d646a1c
De <spark49942@gmail.com>
2022-10-26 18:23:26 +0300

    DZ1

--- smn.py
+++ smn.py
@@ -1,17 +1,3 @@
-# from telegram import Update
-# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-
-# async def howareu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-
-# app = ApplicationBuilder().token('5479458028:AAHnAfaQe6CqI0LNVcFeSaKzXEGp0ygFhxE').build()
-
-# app.add_handler(CommandHandler("howareu", howareu))
-
-# app.run_polling()
-
 from botcmds import howareu, hello, time, summe, helpme
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
