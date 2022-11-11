650a8173a9207440eb920f10acfc69afe473ba16
De <spark49942@gmail.com>
2022-11-11 04:25:39 +0300

    nshrshir

--- smn2.py
+++ smn2.py
@@ -1,57 +1,86 @@
 import telebot
 from spiek import *
 from cmath import *
-
-def calc():
-    import math
-    myfunc = input('Enter a function: ')
-    num = float(input('Enter a number: '))
-    func = getattr(math, myfunc)
-    result = func(num)
-    return result
-
-a=calc()
-print (a)
+import cmath
 
 bot = telebot.TeleBot('5479458028:AAHnAfaQe6CqI0LNVcFeSaKzXEGp0ygFhxE')
 
-print('\nit\'s me, MARIO!')
+print('\nNashira s\'here')
 
 @bot.message_handler(commands=['math'])
 def math(meg: telebot.types.Message):
     with open('db.csv', 'a') as file:
         file.write(f'{meg.from_user.first_name}, {meg.from_user.id}, {meg.text} ({datetime.datetime.now().replace(microsecond=0)})\n')
-    bot.send_message(meg.from_user.id, 'Go ahead, porcupine')
-    bot.register_next_step_handler(meg, work)
+    bot.send_message(meg.from_user.id, 'Hello. Please select mode by typing a proper number:\n(1) Rational\(2) Complex')
+    bot.register_next_step_handler(meg, mode)
+
+def mode(msg: telebot.types.Message):
+    answ = msg.text
+    if answ == '1':
+        bot.send_message(msg.from_user.id, 'Now input your request, please.\nIn this mode, you can sum[+], substract[-], multiply[*] or divide[/]\nany rational numbers in any order, in round brackets [()] too')
+        bot.register_next_step_handler(msg, work)
+    elif answ == '2':
+        bot.send_message(msg.from_user.id, 'Now input your request, please.\nIn this mode, you can use: sin, cos, sqrt functions on a single number')
+        bot.register_next_step_handler(msg, calc)
 
 def work(message: telebot.types.Message):
-    ask = message.text
-    def minus(ask):
-        return ask[0] - ask[1]
+    try:
+        ask = message.text
+        def minus(ask):
+            return ask[0] - ask[1]
 
-    def multi(ask):
-        return ask[0] * ask[1]
+        def multi(ask):
+            return ask[0] * ask[1]
 
-    def divide(ask):
-        return ask[0] / ask[1]
+        def divide(ask):
+            return ask[0] / ask[1]
 
-    def count_from_string(ask):
-        if "(" in ask:
-            bk1 = ask.rindex("(")
-            bk2 = ask.index(")", bk1)
-            return count_from_string(ask[:bk1] + str(count_from_string(ask[bk1 + 1:bk2])) + ask[bk2 + 1:])
-        if ask.isdigit():
-            return int(ask)
-        if "+" in ask:
-            return sum([count_from_string(item) for item in ask.split("+", 1)])
-        if "-" in ask:
-            return minus([count_from_string(item) for item in ask.split("-", 1)])
-        if "/" in ask:
-            return divide([count_from_string(item) for item in ask.split("/", 1)])
-        if "*" in ask:
-            return multi([count_from_string(item) for item in ask.split("*", 1)])
+        def count_from_string(ask):
+            if "(" in ask:
+                bk1 = ask.rindex("(")
+                bk2 = ask.index(")", bk1)
+                return count_from_string(ask[:bk1] + str(count_from_string(ask[bk1 + 1:bk2])) + ask[bk2 + 1:])
+            if ask.isdigit():
+                return int(ask)
+            if "+" in ask:
+                return sum([count_from_string(item) for item in ask.split("+", 1)])
+            if "-" in ask:
+                return minus([count_from_string(item) for item in ask.split("-", 1)])
+            if "/" in ask:
+                return divide([count_from_string(item) for item in ask.split("/", 1)])
+            if "*" in ask:
+                return multi([count_from_string(item) for item in ask.split("*", 1)])
 
-    bot.send_message(message.from_user.id, f'Answer: {round(count_from_string(ask), 2)}')
+        bot.send_message(message.from_user.id, f'Answer: {round(count_from_string(ask), 2)}')
+    except Exception:
+        bot.send_message(message.from_user.id, f'ERROR')
+    bot.register_next_step_handler(message, cycl)
+
+def cycl(hm: telebot.types.Message):
+    bot.send_message(hm.from_user.id, f'New request? y/n')
+    answr = hm.text
+    if answr == 'y':
+        bot.register_next_step_handler(hm, work)
+
+def calc(message: telebot.types.Message):
+    try:
+        ask = message.text.split()
+        func = getattr(cmath, ask[0])
+        result = func(int(ask[1]))
+        bot.send_message(message.from_user.id, f'Answer: {result}')
+    except Exception:
+        bot.send_message(message.from_user.id, f'ERROR')
+    bot.register_next_step_handler(message, cycl)        
+
+# def calc():
+#     import math
+#     myfunc = input('Enter a function: ')
+#     num = float(input('Enter a number: '))
+#     func = getattr(math, myfunc)
+#     result = func(num)
+#     return result
+# print (calc())
+
 # @bot.message_handler(commands=["hi"])
 # def hi(message: telebot.types.Message):
 #     bot.send_message(message.from_user.id, "Hello")
